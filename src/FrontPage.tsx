9fd097f8ff726b8d0d679b112b8370f5ce0c7ad3
Warren Haslam <warren.haslam@gmail.com>
2026-03-11 16:49:28 +0000

    Added placeholder text

--- src/FrontPage.tsx
+++ src/FrontPage.tsx
@@ -13,12 +13,14 @@ const FrontPage = () => {
             className="flex flex-col content-center items-center h-full w-full text-center bg-white text-gray-800 gap-4 py-2">
             <h1>Karasu Software</h1>
             <h2>Software consultancy, application design & engineering</h2>
-            {/*
-            <div className="flex w-full bg-neutral-700 justify-center" >
-                <img src={rookImage} alt="Rook" className="h-48 w-auto" data-id="img" />
-            </div>
-            */}
             <ThreeScene />
+            <div className="my-2 text-xl text-red-600">This is a placeholder!</div>
+            <div className="flex flex-col gap-2 text-base" >
+                <div>Page created using React, Typescript, Tailwind CSS, Vite, pnpm, and Three.js</div>
+                {/*
+                <div>Fonts 'Roboto Slab' & 'Source Sans 3'. Hosted on Cloudflare Pages</div>
+                */}
+            </div>
         </div>
     )
 }
