b09ed40727fd34e675d2f2f588e2f1f28cb7d6e3
Warren Haslam <warren.haslam@gmail.com>
2026-04-01 16:52:20 +0100

    To see if github action runs on PR

--- test/utils/Utils.test.ts
+++ test/utils/Utils.test.ts
@@ -3,14 +3,18 @@ import { isString } from '@/utils/Utils'
 
 // - - - - - Tests - - - - - //
 
-test('isString returns true', () => {
-    expect(isString('hello')).toBe(true)
+describe('String utils', () => {
+
+    test('isString returns true', () => {
+        expect(isString('hello')).toBe(true)
+    })
+
+    test('isString returns true - empty string', () => {
+        expect(isString('')).toBe(true)
+    })
+
+    test('isString returns false', () => {
+        expect(isString(null)).toBe(false)
+    })
 })
 
-test('isString returns true - empty string', () => {
-    expect(isString('')).toBe(true)
-})
-
-test('isString returns false', () => {
-    expect(isString(null)).toBe(false)
-})
