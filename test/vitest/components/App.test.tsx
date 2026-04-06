0015f4e4f80b9fba669ee48a2402fd0edf292089
Warren Haslam <warren.haslam@gmail.com>
2026-04-06 20:37:59 +0100

    Fix for vscode error in test

--- test/vitest/components/App.test.tsx
+++ test/vitest/components/App.test.tsx
@@ -1,13 +1,16 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it/*, expect*/ } from 'vitest'
 import { render, screen } from '@testing-library/react';
 
 import { App } from '@/components/App/App';
 
 // - - - - - Tests - - - - - //
 
+// Error: Error creating WebGL context.
 describe.skip('App component', () => {
-    it('renders the Vite + React heading', () => {
+    it('Renders the heading', async () => {
         render(<App />);
-        expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
-    });
-});
+        //expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
+
+        screen.getByText(/Vite \+ React/i)
+    })
+})
