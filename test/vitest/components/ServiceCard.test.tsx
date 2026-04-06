8a857fa345b1bef4fd808ee7982aac442543f910
Warren Haslam <warren.haslam@gmail.com>
2026-04-06 19:27:36 +0100

    Comment out unused import

--- test/vitest/components/ServiceCard.test.tsx
+++ test/vitest/components/ServiceCard.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it/*, expect*/ } from 'vitest'
 import { render, screen } from '@testing-library/react';
 
 import { ServiceCard } from '@/components/Cards/ServiceCard';
